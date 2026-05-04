36bc8a2490c7e2790a6199509dae7bc9a6d238e0
PaulSam <paulossam409@gmail.com>
2026-05-04 14:53:03 +0300

    feat: add dotenv and cors for environment configuration and CORS support

--- server/server.js
+++ server/server.js
@@ -1,7 +1,20 @@
+// Load Environmental Variables
+const dotenv = require('dotenv')
+dotenv.config()
+
+// Enable CORS
+const cors = require('cors')
+const corsConfig = require('./config/cors.json')
+
+// Initialize Express App
 const express = require('express')
-
 const app = express()
+const PORT = process.env.PORT || 3000
 
-app.listen(3001, () => {
+// Middlewares
+app.use(express.json())
+app.use(cors(corsConfig))
+
+app.listen(PORT, () => {
   console.log("server started")
 })
