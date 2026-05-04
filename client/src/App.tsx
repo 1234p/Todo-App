536139761375e50d9e3b26938ed7bbc4c8e4e95f
PaulSam <paulossam409@gmail.com>
2026-05-04 15:56:47 +0300

    feat: restructure project by removing unused files and updating index.html, App.tsx, and styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,27 @@
 import "./App.scss";
 
 export default function App() {
-  return <></>;
+  return (
+    <div className="todo__content">
+      <header className="todo__header">
+        <h1 className="todo__title">Todo</h1>
+        <div className="todo__theme-switch"></div>
+      </header>
+
+      <p>
+        Todo
+        {/* Add dynamic number */}
+        items left All Active Completed Clear Completed Drag and drop to reorder
+        list
+      </p>
+
+      <footer className="attribution">
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </footer>
+    </div>
+  );
 }
