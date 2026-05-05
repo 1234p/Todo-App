d855f0b6fe42506036d5a1e31c518a75e7a6e769
PaulSam <paulossam409@gmail.com>
2026-05-05 15:48:41 +0300

    feat: implement theme toggle functionality with icons and update styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
 import { FaPlus } from "react-icons/fa";
+import { IoMoon, IoSunny } from "react-icons/io5";
 import "./App.scss";
+
+type theme = "light" | "dark";
 
 export default function App() {
   const [isHover, setIsHover] = useState<boolean>(false);
+  const [theme, setTheme] = useState<theme>("dark");
+
+  const handleToggleTheme = () =>
+    theme === "light" ? setTheme("dark") : setTheme("light");
 
   return (
     <div className="todo__content">
       <header className="todo__header">
         <h1 className="todo__title">Todo</h1>
-        <div className="todo__theme-switch"></div>
+        <div className="todo__theme-switch" onClick={handleToggleTheme}>
+          {theme === "dark" ? <IoSunny /> : <IoMoon />}
+        </div>
       </header>
 
       <div className="todo__add-todo">
