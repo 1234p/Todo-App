49f412b3602a6c1587085808e7f75e39e14033b1
PaulSam <paulossam409@gmail.com>
2026-05-06 13:52:11 +0300

    fix: update response status for getTodos and remove unnecessary console log in createTodo

--- server/controllers/myTodosController.js
+++ server/controllers/myTodosController.js
@@ -3,7 +3,7 @@ const MyTodosModel = require("../models/MyTodos");
 const getTodos = async (req, res) => {
   try {
     const data = await MyTodosModel.find();
-    res.json(data);
+    res.status(201).json(data);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Failed to fetch todos" });
@@ -12,10 +12,11 @@ const getTodos = async (req, res) => {
 
 const createTodo = async (req, res) => {
   try {
-    console.log("req.body:", req.body);
     const { todo } = req.body;
+
     const newTodo = new MyTodosModel({ todo });
     const savedTodo = await newTodo.save();
+
     res.status(201).json(savedTodo);
   } catch (error) {
     console.error(error);
